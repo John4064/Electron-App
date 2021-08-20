ed46883e7dda490d73ab4d617a7d009f37a5932c
John4064 <jparkhurst120@gmail.com>
2021-08-20 00:28:06 -0400

    integrating basic flask app to electron
    Playing around with flask and electron

--- app.js
+++ app.js
@@ -1,19 +1,71 @@
-// note that the fs package does not exist on a normal browser
-const fs = require("fs");
+const { app, BrowserWindow, ipcMain } = require("electron");
+const path = require("path");
 
-//a dialog box module from electron
-const { dialog } = require("electron").remote;
+let mainWindow;
 
-// Also note that document does not exist in a normal node environment
-// button click event
-document.getElementById("mybutton").addEventListener("click", () => {
-    const data = "Successfully wrote to the desktop"; // the data we want to save to the desktop
-
-    //launch save dialog window
-    dialog.showSaveDialog(filename => {
-        //save file at the destination indicated by filename
-        fs.writeFileSync(filename + ".txt", data, "utf-8", () => {
-            console.log("attempted to write to the desktop");
-        });
+// Function to create independent window or main window
+function createWindow() {
+    mainWindow = new BrowserWindow({
+        width: 800,
+        height: 600,
+        // Make sure to add webPreferences with
+        // nodeIntegration and contextIsolation
+        webPreferences: {
+            nodeIntegration: true,
+            contextIsolation: false,
+        },
+        show: false,
     });
+
+// Main window loads index.html file
+    mainWindow.loadFile("index.html");
+
+// To maximize the window
+    mainWindow.maximize();
+    mainWindow.show();
+}
+
+// Function to create child window of parent one
+function createChildWindow() {
+    childWindow = new BrowserWindow({
+        width: 1000,
+        height: 700,
+        modal: true,
+        show: false,
+        parent: mainWindow, // Make sure to add parent window here
+
+        // Make sure to add webPreferences with below configuration
+        webPreferences: {
+            nodeIntegration: true,
+            contextIsolation: false,
+            enableRemoteModule: true,
+        },
+    });
+
+// Child window loads settings.html file
+    childWindow.loadFile("settings.html");
+
+    childWindow.once("ready-to-show", () => {
+        childWindow.show();
+    });
+}
+
+ipcMain.on("openChildWindow", (event, arg) => {
+    createChildWindow();
+});
+
+app.whenReady().then(() => {
+    createWindow();
+
+    app.on("activate", () => {
+        if (BrowserWindow.getAllWindows().length === 0) {
+            createWindow();
+        }
+    });
+});
+
+app.on("window-all-closed", () => {
+    if (process.platform !== "darwin") {
+        app.quit();
+    }
 });
--- main.js
+++ main.js
@@ -1,25 +1,31 @@
 const { app, BrowserWindow } = require('electron');
-const path = require('path')
-
-
+const path = require('path');
 function createWindow () {
     const win = new BrowserWindow({
         width: 800,
         height: 600,
         webPreferences: {
-            preload: path.join(__dirname, 'preload.js')
+            nodeIntegration: true
         }
     })
-    win.loadFile('index.html')
-}
 
-//When the app is ready, then, it will execute the function createWindow
-app.whenReady().then(() => {
-    createWindow()
-});
-// Quit when all windows are closed.
-app.on('window-all-closed', function () {
-    if (process.platform !== 'darwin') {
+    win.loadFile('index.html');
+}
+app.whenReady().then(createWindow)
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') {0
         app.quit()
     }
-});
+    var python = require('child_process').spawn('py', ['./backend/app.py']);
+    python.stdout.on('data', function (data) {
+        console.log("data: ", data.toString('utf8'));
+    });
+    python.stderr.on('data', (data) => {
+        console.log(`stderr: ${data}`); // when error
+    });
+})
+app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+        createWindow()
+    }
+})
